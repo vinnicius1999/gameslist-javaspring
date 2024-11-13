bb34fcc6e1a1b194708a556c198939b3ea903a9e
vinicius barbosa <viniciusbarbosa934@gmail.com>
2024-11-13 15:33:16 -0300

    inserindo no banco de dados

--- src/main/java/com/gameslist/gameslist/entities/Game.java
+++ src/main/java/com/gameslist/gameslist/entities/Game.java
@@ -24,9 +24,14 @@ public class Game {
 	@Column(name ="game_year")
 	private Integer year;
 	private String genre;
-	private String plataform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	
@@ -35,14 +40,15 @@ public class Game {
 	}
 
 
-	public Game(long id, String title, Integer year, String genre, String plataform, String imgUrl,
+	public Game(long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -89,13 +95,23 @@ public class Game {
 	}
 
 
-	public String getPlataform() {
-		return plataform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
 
-	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+
+	public Double getScore() {
+		return score;
+	}
+
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 
